123a01abe1e9737846fbd274685a8377025af6e9
Matt Neale <matt@zappar.com>
2013-12-15 10:33:28 +0000

    added number to string helper function.

--- source/io/iohelper.h
+++ source/io/iohelper.h
@@ -12,6 +12,17 @@ std::vector<std::string> & Split(const std::string & s, char delim, std::vector<
 std::vector<std::string> Split(const std::string &s, char delim);
 
 template<typename T>
+std::string NtoS(T number);
+
+template<typename T>
+std::string NtoS(T number)
+{
+   std::stringstream ss;//create a stringstream
+   ss << number;//add number to the stream
+   return ss.str();//return a string with the contents of the stream
+}
+
+template<typename T>
 T StoN(const std::string& numberAsString);
 
 template<typename T>
